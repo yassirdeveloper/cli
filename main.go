ee08d19c52027905fa0f0328e4a0e18d8396a340
yassirdeveloper <yassirdeveloper@gmail.com>
2025-02-26 00:22:58 +0000

    +++

--- main.go
+++ main.go
@@ -12,10 +12,10 @@ import (
 
 func main() {
 	commander := commands.NewCommander()
-
 	commander.AddCommand("help", commands.HelpCommand)
 	commander.AddCommand("exit", commands.ExitCommand)
 	commander.SetWriter(os.Stdout)
+
 	args := os.Args
 	if len(args) > 1 {
 		err := commander.Run(args[1:])
@@ -25,19 +25,20 @@ func main() {
 		}
 		os.Exit(0)
 	}
+
 	line, err_ := readline.New("> ")
 	if err_ != nil {
 		log.Fatalf("Error initializing readline: %v", err_)
 	}
 	defer line.Close()
 	line.Config.HistoryLimit = 100
+
 	for {
 		input, err_ := line.Readline()
 		if err_ != nil {
 			fmt.Println("\nExiting...") // Exit on EOF (Ctrl+D)
 			break
 		}
-		input = strings.TrimSpace(input)
 		line.SaveHistory(input)
 
 		err := commander.Run(strings.Split(strings.TrimSpace(strings.TrimSuffix(input, "\n")), " "))
